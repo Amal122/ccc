ab589599f1c4ab8720de5e299a3a550ffc3b3100
amal122 <amalagarwal007@gmail.com>
2018-07-26 12:27:59 +0530

    initial commit

---
+++ app/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View,Image,Text} from 'react-native';
+
+const Logo = () =>(
+<View>
+    <Image source ={require ('./images/a.jpeg')}>
+    
+     </Image>   
+<Text>Currency Converter</Text>
+ </View>   
+
+);
+export default Logo;
--- app/components/container/container.js
+++ app/components/container/container.js
@@ -4,7 +4,7 @@ import {View} from 'react-native';
 import style from './style';
 
 const Container = ({children}) =>   (
-<View styles={style.Container}>
+<View style={style.Container}>
 {children}
 
  </View>   
